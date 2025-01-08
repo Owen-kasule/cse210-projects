559085b7a7aa64af0e42c903f3dfcdd249f61fbc
Owen-kasule <owenatug@gmail.com>
2025-01-08 16:37:33 +0300

    Removed exclamation mark from welcome message in DisplayWelcome function.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -17,7 +17,7 @@ class Program
     // Function to display a welcome message
     static void DisplayWelcome()
     {
-        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine("Welcome to the Program");
     }
 
     // Function to prompt for and return the user's name
