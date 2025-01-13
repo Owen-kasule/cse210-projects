902af94be7d8b6fb7a1971ff5280e42e33c7cdd2
Owen-kasule <owenatug@gmail.com>
2025-01-13 13:49:57 +0300

    Refactor Entry class and add PromptGenerator for enhanced journal functionality

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    private List<string> _prompts = new List<string>
+    {
+        "What made you smile today?",
+        "What are you grateful for?",
+        "Describe a moment you felt proud.",
+        "What was the highlight of your day?",
+        "Who is someone you appreciate and why?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
